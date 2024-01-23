1ceb16bfdb3b3a23d392380ca93afdc7dd9ac2e3
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-01-23 23:54:46 +0545

    cors

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const homeRouter = require("./routes/homeRoutes");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 
 app.use("/", homeRouter);
 
